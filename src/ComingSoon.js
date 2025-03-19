4e19cf44338462dd6efa0de5a5c68690040140ff
Shelby Salas <shelbysalas@Shelbys-MBP.lan>
2025-03-18 20:59:06 -0500

    Updated coming soon page

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -3,30 +3,30 @@ import waitImage from "./imgs/wait2.png";
 export const ComingSoon = () => {
   return (
     <div className="countdown-container">
-     
-        <div
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: "100vh",
+          textAlign: "center",
+          padding: "20px", // Add some padding around the content
+        }}
+      >
+        <p style={{ marginTop: "30%" }}>Mary's next vacation:</p>
+        <img
+          src={waitImage}
+          alt="Sunglasses Taylor"
           style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignItems: "center",
-            height: "100vh",
-            textAlign: "center",
-            padding: "20px", // Add some padding around the content
+            width: "100%",
+            maxWidth: "600px",
+            display: "block",
+            margin: "0 auto",
           }}
-        >
-            <p style={{ marginTop: "30%" }}>Mary's next vacation:</p>
-          <h1 style={{ marginBottom: "auto" }}>COMING SOON</h1>
-          <img
-            src={waitImage}
-            alt="Sunglasses Taylor"
-            style={{
-              width: "100%",
-              maxWidth: "600px",
-              display: "block",
-              margin: "0 auto",
-            }} />
-        </div>  
+        />
+        <h1 style={{ marginBottom: "auto" }}>COMING SOON</h1>
+      </div>
     </div>
   );
 };
